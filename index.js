7f125691e17fd3a08f8ee34c7b06e391943899c6
Dex Hewitt <dexhewitt@outlook.com>
2022-08-27 21:05:50 +1000

    add most functionality, bug remains, cannot put more than 2 numbers in without duplication

--- index.js
+++ index.js
@@ -14,37 +14,33 @@ const [plus, minus, times, divide, squared, powerTo, equals, clear, allClear] = 
 
 // Initialising Mutable Variables
 
-let firstArgument = [0]
+let valueCurrent = 0
+let value = [0]
 
 // Operation Event Listeners
 
 plus.addEventListener("click", () => {
-    
-    console.log("clicked")
+    changeCurrentValue()
+    display.textContent += "+"
 })
 
 minus.addEventListener("click", () => {
-    
+    changeCurrentValue()
+    display.textContent += "-"
 })
 
 times.addEventListener("click", () => {
-    
+    changeCurrentValue()
+    display.textContent += "*"
 })
 
 divide.addEventListener("click", () => {
-    
-})
-
-squared.addEventListener("click", () => {
-    
-})
-
-powerTo.addEventListener("click", () => {
-    
+    changeCurrentValue()
+    display.textContent += "/"
 })
 
 equals.addEventListener("click", () => {
-    
+    equalsOperator()
 })
 
 clear.addEventListener("click", () => {
@@ -52,8 +48,9 @@ clear.addEventListener("click", () => {
 })
 
 allClear.addEventListener("click", () => {
-    
+    clearAll()
 })
+
 // Number Event Listeners
 
 one.addEventListener("click", () => {
@@ -96,20 +93,37 @@ zero.addEventListener("click", () => {
     writeNumber(0)
 })
 
-
-
-// Operator Event Listeners
+// Functions
 
 function writeNumber(number) {
-    if(firstArgument == 0){
-        firstArgument = number
+    if(value[0] == 0){
+        value[0] = number
         display.textContent = number
-    }  else {
-        firstArgument = String(firstArgument) + String(number)
-        display.textContent = Number(firstArgument)
-    }
+    }  else if (value[valueCurrent] == 0){
+        value[valueCurrent] = number
+        display.textContent += number 
+        console.log(value)
+    } else {
+        value[valueCurrent] = String(value[valueCurrent]) + String(number) // Convert to string to concatonate
+        display.textContent = display.textContent.slice(0, -1) + Number(value[valueCurrent]) // Convert Back to number //Need to fix
+        console.log(value)
+    } 
 }
 
 function equalsOperator() {
+    valueCurrent = 0
+    value = [0]
+    value[valueCurrent] = eval(display.textContent) 
+    display.textContent = value[valueCurrent]
+}
 
+function clearAll() {
+    value = [0]
+    valueCurrent = 0
+    display.textContent = value[valueCurrent] 
+}
+
+function changeCurrentValue (){
+    valueCurrent++
+    value.push(0)
 }
